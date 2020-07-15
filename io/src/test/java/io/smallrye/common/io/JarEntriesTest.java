d0c9ab8f002cf353861e548611945360800ff435
George Gastaldi <gegastaldi@gmail.com>
2020-07-15 18:10:56 -0300

    Add tests

---
+++ io/src/test/java/io/smallrye/common/io/JarEntriesTest.java
@@ -0,0 +1,22 @@
+package io.smallrye.common.io;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.junit.Test;
+
+public class JarEntriesTest {
+
+    @Test
+    public void shouldUseRealName() throws IOException {
+        File tmpFile = MultiReleaseJarGenerator.generateMultiReleaseJar();
+        JarFile jarFile = JarFiles.create(tmpFile);
+        JarEntry jarEntry = jarFile.getJarEntry("foo.txt");
+        assertEquals(jarEntry.getRealName(), JarEntries.getRealName(jarEntry));
+    }
+
+}
---
+++ io/src/test/java/io/smallrye/common/io/JarFilesTest.java
@@ -0,0 +1,19 @@
+package io.smallrye.common.io;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.jar.JarFile;
+
+import org.junit.Test;
+
+public class JarFilesTest {
+
+    @Test
+    public void shouldReadMultiReleaseJars() throws IOException {
+        File tmpFile = MultiReleaseJarGenerator.generateMultiReleaseJar();
+        JarFile jarFile = JarFiles.create(tmpFile);
+        assertTrue(jarFile.isMultiRelease());
+    }
+}
