8fd104a590471a9233d4adf0a637c173eba2e0a8
George Gastaldi <gegastaldi@gmail.com>
2020-07-16 11:50:53 -0300

    Using JUnit 5

--- io/src/test/java/io/smallrye/common/io/jar/JarEntriesIT.java
+++ io/src/test/java/io/smallrye/common/io/jar/JarEntriesIT.java
@@ -1,23 +1,36 @@
 package io.smallrye.common.io.jar;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
 
 // This needs to be run as an integration-test
 public class JarEntriesIT {
 
     @Test
-    public void shouldUseRealName() throws IOException {
+    @DisabledOnJre(JRE.JAVA_8)
+    public void shouldUseMultiReleaseName() throws IOException {
         File tmpFile = MultiReleaseJarGenerator.generateMultiReleaseJar();
         JarFile jarFile = JarFiles.create(tmpFile);
         JarEntry jarEntry = jarFile.getJarEntry("foo.txt");
         assertEquals("META-INF/versions/9/foo.txt", JarEntries.getRealName(jarEntry));
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_8)
+    public void shouldUseName() throws IOException {
+        File tmpFile = MultiReleaseJarGenerator.generateMultiReleaseJar();
+        JarFile jarFile = JarFiles.create(tmpFile);
+        JarEntry jarEntry = jarFile.getJarEntry("foo.txt");
+        assertEquals("foo.txt", JarEntries.getRealName(jarEntry));
     }
 
 }
--- io/src/test/java/io/smallrye/common/io/jar/JarFilesTest.java
+++ io/src/test/java/io/smallrye/common/io/jar/JarFilesTest.java
@@ -1,16 +1,19 @@
 package io.smallrye.common.io.jar;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.jar.JarFile;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
 
 public class JarFilesTest {
 
     @Test
+    @DisabledOnJre(JRE.JAVA_8)
     public void shouldReadMultiReleaseJars() throws IOException {
         File tmpFile = MultiReleaseJarGenerator.generateMultiReleaseJar();
         JarFile jarFile = JarFiles.create(tmpFile);
