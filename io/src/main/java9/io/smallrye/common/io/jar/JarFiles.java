86adb6af9d0d857fdf280f23b53c50d1399550f4
George Gastaldi <gegastaldi@gmail.com>
2020-07-16 11:58:43 -0300

    Introduced JarFiles#isMultiRelease

--- io/src/main/java9/io/smallrye/common/io/jar/JarFiles.java
+++ io/src/main/java9/io/smallrye/common/io/jar/JarFiles.java
@@ -40,4 +40,13 @@ public class JarFiles {
     public static JarFile create(File file, boolean verify) throws IOException {
         return new JarFile(file, verify, ZipFile.OPEN_READ, JarFile.runtimeVersion());
     }
+
+    /**
+     * Returns true if this {@link JarFile} is a multi-release jar. On Java 8 this is done by browsing the manifest.
+     * On Java 9+, there is a isMultiRelease method
+     */
+    public static boolean isMultiRelease(JarFile jarFile) {
+        return jarFile.isMultiRelease();
+    }
+
 }
--- io/src/test/java/io/smallrye/common/io/jar/JarFilesTest.java
+++ io/src/test/java/io/smallrye/common/io/jar/JarFilesTest.java
@@ -7,16 +7,13 @@ import java.io.IOException;
 import java.util.jar.JarFile;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnJre;
-import org.junit.jupiter.api.condition.JRE;
 
 public class JarFilesTest {
 
     @Test
-    @DisabledOnJre(JRE.JAVA_8)
     public void shouldReadMultiReleaseJars() throws IOException {
         File tmpFile = MultiReleaseJarGenerator.generateMultiReleaseJar();
         JarFile jarFile = JarFiles.create(tmpFile);
-        assertTrue(jarFile.isMultiRelease());
+        assertTrue(JarFiles.isMultiRelease(jarFile));
     }
 }
