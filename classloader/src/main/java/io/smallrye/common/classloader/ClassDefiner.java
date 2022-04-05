70639b1500d5d60bd19f8944533028031e25c154
David M. Lloyd <david.lloyd@redhat.com>
2022-04-05 09:20:38 -0500

    Require Java 11 (minimum) at run time

--- classloader/src/main/java/io/smallrye/common/classloader/ClassDefiner.java
+++ classloader/src/main/java/io/smallrye/common/classloader/ClassDefiner.java
@@ -1,69 +1,26 @@
 package io.smallrye.common.classloader;
 
 import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-import java.security.ProtectionDomain;
-
-import sun.misc.Unsafe;
 
 public class ClassDefiner {
-    private static final Unsafe unsafe;
-    private static final Method defineClass;
-
-    static {
-        unsafe = AccessController.doPrivileged(new PrivilegedAction<Unsafe>() {
-            public Unsafe run() {
-                try {
-                    final Field field = Unsafe.class.getDeclaredField("theUnsafe");
-                    field.setAccessible(true);
-                    return (Unsafe) field.get(null);
-                } catch (IllegalAccessException e) {
-                    throw new IllegalAccessError(e.getMessage());
-                } catch (NoSuchFieldException e) {
-                    throw new NoSuchFieldError(e.getMessage());
-                }
-            }
-        });
-
-        defineClass = AccessController.doPrivileged(new PrivilegedAction<Method>() {
-            @Override
-            public Method run() {
-                try {
-                    return Unsafe.class.getMethod("defineClass", String.class, byte[].class, int.class, int.class,
-                            ClassLoader.class, ProtectionDomain.class);
-                } catch (NoSuchMethodException e) {
-                    throw new NoSuchMethodError(e.getMessage());
-                }
-            }
-        });
-    }
-
     public static Class<?> defineClass(MethodHandles.Lookup lookup, Class<?> parent, String className, byte[] classBytes) {
         SecurityManager sm = System.getSecurityManager();
         if (sm != null) {
             sm.checkPermission(DefineClassPermission.getInstance());
         }
 
-        // The className for Unsafe.defineClass must match the one in classBytes, so we can try to verify the package
-        // with the parent
-        String parentPkg = parent.getPackage().getName();
-        String classPkg = className.substring(0, className.lastIndexOf('.'));
-
-        if (!parentPkg.equals(classPkg)) {
-            throw new IllegalArgumentException("Class not in same package as lookup class");
-        }
-
-        try {
-            return (Class<?>) defineClass.invoke(unsafe, className, classBytes, 0, classBytes.length, parent.getClassLoader(),
-                    null);
-        } catch (IllegalAccessException e) {
-            throw new IllegalAccessError(e.getMessage());
-        } catch (InvocationTargetException e) {
-            throw new IllegalStateException(e.getMessage());
-        }
+        return AccessController.doPrivileged(new PrivilegedAction<Class<?>>() {
+            @Override
+            public Class<?> run() {
+                try {
+                    MethodHandles.Lookup privateLookupIn = MethodHandles.privateLookupIn(parent, lookup);
+                    return privateLookupIn.defineClass(classBytes);
+                } catch (IllegalAccessException e) {
+                    throw new IllegalAccessError(e.getMessage());
+                }
+            }
+        });
     }
 }
--- cpu/src/main/java/io/smallrye/common/cpu/ProcessorInfo.java
+++ cpu/src/main/java/io/smallrye/common/cpu/ProcessorInfo.java
@@ -1,88 +1,13 @@
 package io.smallrye.common.cpu;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-import java.util.Locale;
-
 /**
- * Provides general information about the processors on this host.
+ * Provides general information about the processors on this host (Java 9 version).
  */
 public class ProcessorInfo {
     private ProcessorInfo() {
     }
 
-    private static final String CPUS_ALLOWED = "Cpus_allowed:";
-
-    /**
-     * Returns the number of processors available to this process. On most operating systems this method
-     * simply delegates to {@link Runtime#availableProcessors()}. However, on Linux, this strategy
-     * is insufficient, since the JVM does not take into consideration the process' CPU set affinity
-     * which is employed by cgroups and numactl. Therefore this method will analyze the Linux proc filesystem
-     * to make the determination. Since the CPU affinity of a process can be change at any time, this method does
-     * not cache the result. Calls should be limited accordingly.
-     * <br>
-     * Note tha on Linux, both SMT units (Hyper-Threading) and CPU cores are counted as a processor.
-     *
-     * @return the available processors on this system.
-     */
     public static int availableProcessors() {
-        if (System.getSecurityManager() != null) {
-            return AccessController.doPrivileged((PrivilegedAction<Integer>) () -> Integer.valueOf(determineProcessors()))
-                    .intValue();
-        }
-
-        return determineProcessors();
-    }
-
-    private static int determineProcessors() {
-        int javaProcs = Runtime.getRuntime().availableProcessors();
-        if (!isLinux()) {
-            return javaProcs;
-        }
-
-        int maskProcs = 0;
-
-        try {
-            maskProcs = readCPUMask();
-        } catch (Exception e) {
-            // yum
-        }
-
-        return maskProcs > 0 ? Math.min(javaProcs, maskProcs) : javaProcs;
-    }
-
-    private static int readCPUMask() throws IOException {
-        try (FileInputStream stream = new FileInputStream("/proc/self/status")) {
-            try (InputStreamReader inputReader = new InputStreamReader(stream, StandardCharsets.US_ASCII)) {
-                try (BufferedReader reader = new BufferedReader(inputReader)) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        if (line.startsWith(CPUS_ALLOWED)) {
-                            int count = 0;
-                            int start = CPUS_ALLOWED.length();
-                            for (int i = start; i < line.length(); i++) {
-                                final int v = Character.digit(line.charAt(i), 16);
-                                if (v != -1) {
-                                    count += Integer.bitCount(v);
-                                }
-                            }
-                            return count;
-                        }
-                    }
-                }
-            }
-        }
-
-        return -1;
-    }
-
-    private static boolean isLinux() {
-        String osArch = System.getProperty("os.name", "unknown").toLowerCase(Locale.US);
-        return (osArch.contains("linux"));
+        return Runtime.getRuntime().availableProcessors();
     }
 }
