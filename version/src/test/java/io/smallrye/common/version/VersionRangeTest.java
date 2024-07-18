d88dbd1dd9a2f54f342c3c53180f6c6dccc9ec76
George Gastaldi <gegastaldi@gmail.com>
2024-07-18 16:03:45 -0300

    Add more tests

--- version/src/test/java/io/smallrye/common/version/VersionRangeTest.java
+++ version/src/test/java/io/smallrye/common/version/VersionRangeTest.java
@@ -54,4 +54,45 @@ class VersionRangeTest {
     public void testUnboundedRange() {
         assertThrows(IllegalArgumentException.class, () -> VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[1.0,2.0"));
     }
+
+    @Test
+    public void testAlphaVersion() {
+        VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[1.0,)");
+        assertFalse(versionRange.test("1.0.0.Alpha"));
+    }
+
+    @Test
+    public void testAlphaVersionInbound() {
+        VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[1.0.0.Alpha1,)");
+        assertTrue(versionRange.test("1.0.0.Alpha1"));
+        assertTrue(versionRange.test("1.0.0.Beta"));
+    }
+
+    @Test
+    public void testAlphaVersionInboundExclusive() {
+        VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "(1.0.0.Alpha1,)");
+        assertFalse(versionRange.test("1.0.0.Alpha1"));
+        assertTrue(versionRange.test("1.0.0.Beta"));
+    }
+
+    @Test
+    public void testMultipleRanges() {
+        VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "(,1.0],[1.2,)");
+        // Should return true for Versions up to 1.0 (included) and 1.2 or higher
+        assertTrue(versionRange.test("1.0.0.Alpha1"));
+        assertTrue(versionRange.test("1.0.0"));
+        assertFalse(versionRange.test("1.1.0"));
+        assertTrue(versionRange.test("1.2.0"));
+        assertFalse(versionRange.test("1.2.0.Alpha1"));
+    }
+
+    @Test
+    public void testQualifiers() {
+        VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[3.8,3.8.5)");
+        assertTrue(versionRange.test("3.8.4.SP1-redhat-00001"));
+        assertTrue(versionRange.test("3.8.4.SP2-redhat-00001"));
+        assertTrue(versionRange.test("3.8.4.redhat-00002"));
+        assertFalse(versionRange.test("3.8.5.redhat-00003"));
+    }
+
 }
