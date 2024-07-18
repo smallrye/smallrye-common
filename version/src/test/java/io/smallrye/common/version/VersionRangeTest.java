2ac85cd613fd3df8062382cab2e172c808c08391
George Gastaldi <gegastaldi@gmail.com>
2024-07-18 20:30:56 -0300

    Using AssertJ for better readability

--- version/src/test/java/io/smallrye/common/version/VersionRangeTest.java
+++ version/src/test/java/io/smallrye/common/version/VersionRangeTest.java
@@ -1,6 +1,7 @@
 package io.smallrye.common.version;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -10,97 +11,84 @@ class VersionRangeTest {
     @Test
     void testVersionRangeWithInclusive() {
         VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[1.0,2.0]");
-        assertTrue(versionRange.test("1.0.0"));
-        assertTrue(versionRange.test("1.1.0"));
-        assertTrue(versionRange.test("1.899.0"));
-        assertTrue(versionRange.test("2.0"));
-        assertTrue(versionRange.test("2.0.0"));
-        assertFalse(versionRange.test("2.0.1"));
+        assertThat(versionRange)
+                .accepts("1.0.0", "1.1.0", "1.899.0", "2.0", "2.0.0")
+                .rejects("2.0.1");
     }
 
     @Test
     void testVersionRangeWithExclusive() {
         VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "(1.0,2.0)");
-        assertFalse(versionRange.test("1.0.0"));
-        assertTrue(versionRange.test("1.1.0"));
-        assertTrue(versionRange.test("1.899.0"));
-        assertFalse(versionRange.test("2.0"));
-        assertFalse(versionRange.test("2.0.0"));
-        assertFalse(versionRange.test("2.0.1"));
+        assertThat(versionRange)
+                .accepts("1.1.0", "1.899.0")
+                .rejects("1.0.0", "2.0", "2.0.0", "2.0.1");
     }
 
     @Test
     void testVersionRangeWithLowerBoundExclusive() {
         VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "(1.0,2.0]");
-        assertFalse(versionRange.test("1.0.0"));
-        assertTrue(versionRange.test("1.1.0"));
-        assertTrue(versionRange.test("1.899.0"));
-        assertTrue(versionRange.test("2.0"));
-        assertTrue(versionRange.test("2.0.0"));
-        assertFalse(versionRange.test("2.0.1"));
+        assertThat(versionRange)
+                .accepts("1.1.0", "1.899.0", "2.0", "2.0.0")
+                .rejects("1.0.0", "2.0.1");
     }
 
     @Test
     void testVersionRangeWithUpperBoundExclusive() {
         VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[1.0,2.0)");
-        assertTrue(versionRange.test("1.0.0"));
-        assertTrue(versionRange.test("1.1.0"));
-        assertTrue(versionRange.test("1.899.0"));
-        assertFalse(versionRange.test("2.0"));
-        assertFalse(versionRange.test("2.0.0"));
-        assertFalse(versionRange.test("2.0.1"));
+        assertThat(versionRange)
+                .accepts("1.0.0", "1.1.0", "1.899.0")
+                .rejects("2.0", "2.0.0", "2.0.1");
     }
 
     @Test
     public void testUnboundedRange() {
-        assertThrows(IllegalArgumentException.class, () -> VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[1.0,2.0"));
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[1.0,2.0"))
+                .withMessageStartingWith("SRCOM03011");
     }
 
     @Test
     public void testAlphaVersion() {
         VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[1.0,)");
-        assertFalse(versionRange.test("1.0.0.Alpha"));
+        assertThat(versionRange).rejects("1.0.0.Alpha1");
     }
 
     @Test
     public void testAlphaVersionInbound() {
         VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[1.0.0.Alpha1,)");
-        assertTrue(versionRange.test("1.0.0.Alpha1"));
-        assertTrue(versionRange.test("1.0.0.Beta"));
+        assertThat(versionRange).accepts("1.0.0.Alpha1", "1.0.0.Beta1");
     }
 
     @Test
     public void testAlphaVersionInboundExclusive() {
         VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "(1.0.0.Alpha1,)");
-        assertFalse(versionRange.test("1.0.0.Alpha1"));
-        assertTrue(versionRange.test("1.0.0.Beta"));
+        assertThat(versionRange)
+                .accepts("1.0.0.Beta")
+                .rejects("1.0.0.Alpha1");
     }
 
     @Test
     public void testMultipleRanges() {
         VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "(,1.0],[1.2,)");
         // Should return true for Versions up to 1.0 (included) and 1.2 or higher
-        assertTrue(versionRange.test("1.0.0.Alpha1"));
-        assertTrue(versionRange.test("1.0.0"));
-        assertFalse(versionRange.test("1.1.0"));
-        assertTrue(versionRange.test("1.2.0"));
-        assertFalse(versionRange.test("1.2.0.Alpha1"));
+        assertThat(versionRange)
+                .accepts("1.0.0.Alpha1", "1.0.0", "1.2.0")
+                .rejects("1.1.0", "1.2.0.Alpha1");
     }
 
     @Test
     public void testQualifiers() {
         VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[3.8,3.8.5)");
-        assertTrue(versionRange.test("3.8.4.SP1-redhat-00001"));
-        assertTrue(versionRange.test("3.8.4.SP2-redhat-00001"));
-        assertTrue(versionRange.test("3.8.4.redhat-00002"));
-        assertFalse(versionRange.test("3.8.5.redhat-00003"));
+        assertThat(versionRange)
+                .accepts("3.8.4.SP1-redhat-00001", "3.8.4.SP2-redhat-00001", "3.8.4.redhat-00002")
+                .rejects("3.8.5.redhat-00003");
     }
 
     @Test
     @Disabled("This test is failing")
     public void testRangeQualifier() {
         VersionRange versionRange = VersionRange.createFromVersionSpec(VersionScheme.MAVEN, "[3.8.0.redhat-00001,)");
-        assertTrue(versionRange.test("3.8.0.SP1-redhat-00001"));
+        assertThat(versionRange).accepts("3.8.0.SP1-redhat-00001");
     }
 
 }
