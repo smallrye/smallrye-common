d0a1d9d978e30d3cd957a549b35ce700bcc56e85
João Lopes <joao.c.lopes@sapo.pt>
2025-06-02 21:58:17 +0100

    Improved JavaDoc of SuppressForbidden annotation.

--- annotation/src/main/java/io/smallrye/common/annotation/SuppressForbidden.java
+++ annotation/src/main/java/io/smallrye/common/annotation/SuppressForbidden.java
@@ -6,10 +6,17 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Used to suppress forbidden-apis errors inside the element it's added to.
+ * Indicate to code checkers that the annotated usage of a forbidden API
+ * should be allowed even if there is a policy forbidding it.
+ * <p>
+ * A valid reason must be given for this kind of exception; therefore,
+ * the {@link #reason() reason} element is required.
  */
 @Retention(RetentionPolicy.CLASS)
-@Target({ ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.METHOD, ElementType.TYPE })
+@Target({ ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.TYPE })
 public @interface SuppressForbidden {
+    /**
+     * {@return the reason for allowing a forbidden API in the annotated element}
+     */
     String reason();
 }
