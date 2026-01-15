80d2b8753bf5942c5f55191f51f1020e684f68a9
Julien Ponge <jponge@ibm.com>
2026-01-15 21:27:27 +0100

    Support both Vert.x 4 and Vert.x 5 contexts

---
+++ vertx4-context/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * Utility classes relating to Vert.x context manipulation.
+ */
+module io.smallrye.common.vertx {
+    requires io.smallrye.common.constraint;
+    requires io.vertx.core;
+
+    exports io.smallrye.common.vertx;
+}
