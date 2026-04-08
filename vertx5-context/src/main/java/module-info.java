9fb3d9949ad373ebf0c1e791fbf372de0ffdf403
Julien Ponge <jponge@ibm.com>
2026-04-08 18:24:55 +0200

    Reworked Vert.x 5 ContextLocal lifecycle hooks
    This should ensure that the 2 ContextLocal fields in VertxContext
    always get picked up by any VertxServiceProvider handler.
    
    Also bumped to Vert.x 5.0.10.

--- vertx5-context/src/main/java/module-info.java
+++ vertx5-context/src/main/java/module-info.java
@@ -8,5 +8,5 @@ module io.smallrye.common.vertx {
     exports io.smallrye.common.vertx;
 
     provides io.vertx.core.spi.VertxServiceProvider
-            with io.smallrye.common.vertx.internal.ContextLocalLoader;
+            with io.smallrye.common.vertx.VertxContext.Loader;
 }
