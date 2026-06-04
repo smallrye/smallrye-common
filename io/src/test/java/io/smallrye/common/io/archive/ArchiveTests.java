565a18e8103450994bc4d94d85ddfaf58538b8fe
David M. Lloyd <dmlloyd@ibm.com>
2026-06-04 09:24:48 -0500

    Restructure some tests and remove old stuff

--- io/src/test/java/io/smallrye/common/io/archive/ArchiveTests.java
+++ io/src/test/java/io/smallrye/common/io/archive/ArchiveTests.java
@@ -1,4 +1,4 @@
-package io.smallrye.common.archive;
+package io.smallrye.common.io.archive;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
