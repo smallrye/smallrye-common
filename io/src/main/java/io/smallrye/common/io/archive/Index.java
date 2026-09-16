2702f1138c2ffc96800fee704410029906b92c50
David M. Lloyd <dmlloyd@ibm.com>
2026-09-16 13:16:25 -0500

    Fix integer truncation bug for huge JAR files

--- io/src/main/java/io/smallrye/common/io/archive/Index.java
+++ io/src/main/java/io/smallrye/common/io/archive/Index.java
@@ -226,7 +226,7 @@ abstract class Index {
                     throw new IllegalArgumentException(
                             "Invalid archive (bad central directory entry signature at offset " + pos + ")");
                 }
-                table[i] = (int) pos;
+                table[i] = pos;
                 pos += data.cdeEntrySize(pos);
             }
             this.table = mergeSort(data, table, new long[(int) count]);
